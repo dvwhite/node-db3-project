f3419d2e55e48156a91218d87454dc8cd78c4a71
David White <dwhite0627@gmail.com>
2020-04-23 21:36:51 -0400

    Add the add db helper function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,3 +1,14 @@
+const db = require("./../data/dbConfig");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
 function find() {
   return db("schemes");
 }
@@ -6,13 +17,35 @@ function findById(id) {
   return db("schemes")
     .where({ id })
     .first()
-    .then((res) => {
-      return res !== undefined ? res : null;
+    .then((record) => {
+      return record !== undefined ? record : null;
     });
 }
+
 function findSteps(id) {
   return db("steps AS s")
     .select(["s.id", "s.step_number", "s.instructions", "schemes.scheme_name"])
     .where("s.scheme_id", id)
     .leftJoin("schemes", "s.scheme_id", "schemes.id");
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .delete()
+    .then((record) => (record === 1 ? record : null));
 }
