d76c4a7ecaeee905f33fa6a7d7abf8b00cc139df
David White <dwhite0627@gmail.com>
2020-04-23 20:33:25 -0400

    Add findSteps to return steps for a scheme, and replace scheme_id with scheme_name

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,3 +10,9 @@ function findById(id) {
       return res !== undefined ? res : null;
     });
 }
+function findSteps(id) {
+  return db("steps AS s")
+    .select(["s.id", "s.step_number", "s.instructions", "schemes.scheme_name"])
+    .where("s.scheme_id", id)
+    .leftJoin("schemes", "s.scheme_id", "schemes.id");
+}
