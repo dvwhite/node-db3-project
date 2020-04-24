5ca2d52cadf726131fc698719f6b7671d2f52533
David White <dwhite0627@gmail.com>
2020-04-23 20:32:51 -0400

    Add the findById helper
    ## Returns the scheme with id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,3 +1,12 @@
 function find() {
   return db("schemes");
 }
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first()
+    .then((res) => {
+      return res !== undefined ? res : null;
+    });
+}
